19fcd7c1897a083b4d421e00a656b62234d5d331
abdul-busybrain <awwalhajine@gmail.com>
2024-09-18 11:23:37 +0100

    I will come for you again

--- src/App.js
+++ src/App.js
@@ -21,18 +21,61 @@ const initialList = [
   },
 ];
 
-function Button({ children }) {
-  return <button className="btn">{children}</button>;
+function Button({ children, onClick }) {
+  return (
+    <button className="btn" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default function App() {
+  console.log("Rendering App");
+
   // NOTE Managing Array of Objects
   const [items, setItems] = useState(initialList);
 
+  // NOTE Boolean flags
+  const [showAddItem, setShowAddItem] = useState(false);
+  const [showPaymentForm, setShowPaymentForm] = useState(false);
+
+  function handleAddItem(newItem) {
+    setItems((prevItems) => [...prevItems, newItem]);
+    setShowAddItem(false);
+  }
+
+  function handleShowAddItem() {
+    console.log("Toggling showAddItem");
+    setShowAddItem((show) => !show);
+  }
+
+  function handleShowPaymentForm() {
+    setShowPaymentForm((show) => !show);
+  }
+
   return (
     <div className="app">
-      <ShoppingItems items={items} />
-      <Button>Add Item</Button>
+      <div className="main-content">
+        <div className="shopping-list">
+          <ShoppingItems items={items} />
+
+          {showAddItem && (
+            <FormAddItem onAddItem={(newItem) => handleAddItem(newItem)} />
+          )}
+
+          <Button onClick={handleShowAddItem}>
+            {showAddItem ? "Close" : "Add Item"}
+          </Button>
+
+          <Button onClick={handleShowPaymentForm}>Pay</Button>
+        </div>
+
+        {showPaymentForm && (
+          <div className="payment-form">
+            <PaymentForm />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
@@ -54,7 +97,9 @@ function Item({ item }) {
     <>
       <li>
         <div className="item">
-          <img src={item.image} alt={item.name} />
+          {(item.image || item.imageURL) && (
+            <img src={item.image || item.imageURL} alt={item.name} />
+          )}
           <div className="item-info">
             <h3>{item.name}</h3>
             <p>
@@ -81,6 +126,74 @@ function Item({ item }) {
   );
 }
 
-function AddItemForm() {}
+function FormAddItem({ onAddItem }) {
+  console.log("FormAddItem rendered");
 
-function PaymentForm() {}
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    console.log("Form submitted");
+
+    e.preventDefault();
+
+    if (!name || !image) return;
+    const id = Date.now();
+    const newItem = {
+      id,
+      name,
+      image,
+      price: 2000,
+    };
+    onAddItem(newItem);
+
+    setName("");
+    setImage("");
+  }
+  return (
+    <>
+      <form className="add-item" onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Ear pod"
+        />
+
+        <label>Image URL:</label>
+        <input
+          type="url"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+      </form>
+      <Button onClick={handleSubmit}>Submit</Button>
+    </>
+  );
+}
+
+function PaymentForm() {
+  return (
+    <>
+      <Button>Pay with wallet</Button>
+      <p>Or pay with card</p>
+      <label>Email:</label>
+      <input type="email" />
+      <label>Card information</label>
+      <input type="text" placeholder="1234 1234 1234 1234" />
+      <input type="date" placeholder="MM / YY" />{" "}
+      <input type="text" placeholder="CVC" />
+      <label>Name on card</label>
+      <input type="text" placeholder="Isa Musa" />
+      <label>Country in Africa</label>
+      <select>
+        <option>Nigeria</option>
+        <option>Niger</option>
+        <option>Benin Republic</option>
+        <option>Egypt</option>
+      </select>
+      <Button>Submit</Button>
+    </>
+  );
+}
